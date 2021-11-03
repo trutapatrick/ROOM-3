0589cda40684046e2c05d3f002b018a0fa640e2d
trutapatrick <patrickpatrick184@yahoo.com>
2021-11-03 21:33:22 +0200

    comentarii

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("1.SignUp");
         User user =new User();
-
         System.out.println("Introduceti username");
         String username=sc.nextLine();
         user.setUsername(username);
@@ -25,7 +24,22 @@ public class Main {
         userRepository.createUser(user);
 
 
-       // System.out.println("2.Transactie");
+        /**1.LoginIn-
+        user si parola
+                    if user exists ->  a. show accounts
+                                       b. make transaction
+                                        c. show transactions history for specific account
+                                        d. log off (send the client to the main menu)
+                    if user doesn't exist -> go to option 2 from the main menu
+         2.SignUp
+         "Enter your citizen ID"
+                    if client doesn't exist -> "Please go to the nearest bank branch to enroll as a client"
+                    if client exists -> "Please enter a username" //////////(that username doesn't already exist)
+                                        "Please enter a password (to be remembered)"
+                           confirmare creare user nou + AUTO_LOG_IN (sa-l trimit in main menu 1. user exists)
+
+         */
+
         HibernateUtil.shutdown();
     }
 }
